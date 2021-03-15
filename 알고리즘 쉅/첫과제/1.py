997955b2cbc3055732de6efb8d1a7dc1750e615e
jeongyeob97 <leejeongyeob97@gmail.com>
2021-03-15 20:29:38 +0900

    알고리즘 쉅 1번 문제 풀이 수정 및 2번 문제 풀이

--- 알고리즘 쉅/첫과제/1.py
+++ 알고리즘 쉅/첫과제/1.py
@@ -1,26 +1,3 @@
-# import sys
-# input = sys.stdin.readline
-#
-# num = int(input())
-# stock_array = list(map(int,input().split()))
-# maximum = float('-inf')
-# price = [0,0]
-#
-# for i in range(0,len(stock_array)-1):
-#     for j in range(i+1,len(stock_array)):
-#         if (stock_array[i] < stock_array[j]) and (maximum < (stock_array[j] - stock_array[i])):
-#             maximum = stock_array[j] - stock_array[i]
-#             price[0], price[1] = stock_array[i], stock_array[j]
-#
-# if maximum == float('-inf'):
-#     print(-1)
-# else:
-#     print(maximum)
-#     print(*price)
-
-
-# 우리 한번 디피로 풀어보아요
-
 n = int(input())
 
 list1 = list(map(int, input().split()))
@@ -30,7 +7,8 @@ for i in range(1, len(list1)):
         temp[i] = [list1[i], list1[i - 1]]
         list1[i] = list1[i - 1]
 
-answer_list = sorted(temp, key=lambda x: x[0] - x[1])
+answer_list = sorted(temp, key=lambda x: x[0], reverse = True)
+answer_list = sorted(answer_list, key=lambda x: x[0] - x[1])
 
 if answer_list[-1][0] - answer_list[-1][1] == 0:
     print(-1)
@@ -38,4 +16,3 @@ else:
     answer = answer_list.pop()
     print(answer[0] - answer[1])
     print(answer[1], answer[0])
-
--- 알고리즘 쉅/첫과제/2.py
+++ 알고리즘 쉅/첫과제/2.py
@@ -1,15 +1,12 @@
-# 정렬을 할 필요는 없구나 아닌가?
-# 이게 max를 구해야 되는 문젠가?
-# 현재 시점에서 이걸 젤 비싸게 팔 수 있는 지점을 찾아야돼
-# 결국엔 그 짓거리를 해야 된다는 건데 굳이 ?
-# 과연 그게
-# 십만 이하면 솔직히 할만 한다
-# 근데 이게 쌉가능인가
-# 한 칸 갈때마다 굳이
-#
-# 한번 해서 가장 큰 값을 찾고
-# 그에
-#
-# 맨 마지막까지 갔는데 답이 없다?
-# 기존의 꺼랑 똑같다 이럼 말이 달라지지
-# 바로 사요나라 손절 치고
+n = int(input())
+list1 = list(map(int,input().split()))
+answer = [0] * len(list1)
+
+for i in range(1,len(list1)):
+    for j in range(i-1,-1,-1):
+        if list1[i] > list1[j]:
+            answer[j] = max(answer[j], list1[i]-list1[j])
+        else:
+            break
+
+print(sum(answer))
