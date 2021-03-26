ce0db7a307ea677f1aa6f11566303dc0110b1ec5
jeongyeob97 <leejeongyeob97@gmail.com>
2021-03-27 01:06:07 +0900

    알고리즘 쉅 두번째 과제 첫번째 문제 풀이

---
+++ 알고리즘 쉅/두번째과제/1.py
@@ -0,0 +1,27 @@
+m,e,d,n = map(int,input().split())
+
+# def recur(a,k,n):
+#     print(a,k,n)
+#     if k == 1:
+#         return a%n
+#     elif k%2 == 0:
+#         temp = ((a**(2/k)%n)**2) % n
+#         print(temp)
+#         return recur(temp,k//2,n)
+#     else:
+#         temp = (a*((a**(k-1)) % n)) % n
+#         print(temp)
+#         return recur(temp,k-1,n)
+
+def cal(a, k, n):
+    if k == 1:
+        return a%n
+    elif k%2 == 0:
+        return (((a**(2/k))%n)**2)%n
+    else:
+        return (a*(a**(k-1) % n))%n
+
+c = cal(m,e,n)
+answer2 = cal(c,d,n)
+print(c)
+print(answer2)
